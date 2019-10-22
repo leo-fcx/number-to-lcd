4489c746d833b99ad771a4d2e2cf99dfa4400c7a
Leo <leo.fcx@gmail.com>
2019-10-22 01:32:35 -0400

    REFACTOR: Improve methods and providing better names

--- src/numberToLCD.spec.js
+++ src/numberToLCD.spec.js
@@ -2,57 +2,57 @@ import { NumberToLCD } from './numberToLCD'
 import assert from 'power-assert'
 
 describe('Number to LCD', () => {
-  it('should be defined', () => {
-    assert(NumberToLCD.convert)
+  it('should defined method to convert specific digit', () => {
+    assert(NumberToLCD.convertDigit);
   });
 
   it('should convert 0 to LCD representation', () => {
     let expected = ' - \n| |\n| |\n| |\n - ';
-    assert.equal(NumberToLCD.convert(0), expected);
+    assert.equal(NumberToLCD.convertDigit(0), expected);
   });
 
   it('should convert 1 to LCD representation', () => {
     let expected = '  |\n  |\n  |\n  |\n  |';
-    assert.equal(NumberToLCD.convert(1), expected);
+    assert.equal(NumberToLCD.convertDigit(1), expected);
   });
 
   it('should convert 2 to LCD representation', () => {
     let expected = ' - \n  |\n - \n|  \n - ';
-    assert.equal(NumberToLCD.convert(2), expected);
+    assert.equal(NumberToLCD.convertDigit(2), expected);
   });
 
   it('should convert 3 to LCD representation', () => {
     let expected = ' - \n  |\n - \n  |\n - ';
-    assert.equal(NumberToLCD.convert(3), expected);
+    assert.equal(NumberToLCD.convertDigit(3), expected);
   });
 
   it('should convert 4 to LCD representation', () => {
     let expected = '| |\n| |\n - \n  |\n  |';
-    assert.equal(NumberToLCD.convert(4), expected);
+    assert.equal(NumberToLCD.convertDigit(4), expected);
   });
 
   it('should convert 5 to LCD representation', () => {
     let expected = ' - \n|  \n - \n  |\n - ';
-    assert.equal(NumberToLCD.convert(5), expected);
+    assert.equal(NumberToLCD.convertDigit(5), expected);
   });
 
   it('should convert 6 to LCD representation', () => {
     let expected = ' - \n|  \n - \n| |\n - ';
-    assert.equal(NumberToLCD.convert(6), expected);
+    assert.equal(NumberToLCD.convertDigit(6), expected);
   });
 
   it('should convert 7 to LCD representation', () => {
     let expected = ' - \n  |\n  |\n  |\n  |';
-    assert.equal(NumberToLCD.convert(7), expected);
+    assert.equal(NumberToLCD.convertDigit(7), expected);
   });
 
   it('should convert 8 to LCD representation', () => {
     let expected = ' - \n| |\n - \n| |\n - ';
-    assert.equal(NumberToLCD.convert(8), expected);
+    assert.equal(NumberToLCD.convertDigit(8), expected);
   });
 
   it('should convert 9 to LCD representation', () => {
     let expected = ' - \n| |\n - \n  |\n - ';
-    assert.equal(NumberToLCD.convert(9), expected);
+    assert.equal(NumberToLCD.convertDigit(9), expected);
   });
 });
