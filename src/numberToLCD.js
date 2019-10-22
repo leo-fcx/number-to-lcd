c406ab16faf2e507c5026e293f047b684b5e00c2
Leo <leo.fcx@gmail.com>
2019-10-21 21:47:42 -0400

    REFACTOR: Number-to-LCD should convert 2 to LCD representation

--- src/numberToLCD.js
+++ src/numberToLCD.js
@@ -5,16 +5,11 @@ export class NumberToLCD {
       ['  |', '  |', '  |', '  |', '  |'],
       [' - ', '  |', ' - ', '|  ', ' - '],
     ];
+    const digit = numbers[number];
 
-    switch (number) {
-      case 0:
-        return numbers[0].join('\n');
-      case 1:
-        return numbers[1].join('\n');
-      case 2:
-        return numbers[2].join('\n');
-      default:
-        return '';
-    }
+    if (digit)
+      return digit.join('\n');
+
+    return '';
   }
 }
