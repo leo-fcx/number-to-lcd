097c0cf73740007a37729a495531e9ba0d8fd913
Leo <leo.fcx@gmail.com>
2019-10-21 21:34:11 -0400

    GREEN: Number-to-LCD should convert 2 to LCD representation

--- src/numberToLCD.js
+++ src/numberToLCD.js
@@ -1,7 +1,9 @@
 export class NumberToLCD {
   static convert(number) {
-    if (number)
+    if (number === 1)
       return '  |\n  |\n  |\n  |\n  |\n';
+    else if (number === 2)
+      return ' - \n  |\n - \n|  \n - \n';
 
     return ' - \n| |\n| |\n| |\n - \n';
   }
